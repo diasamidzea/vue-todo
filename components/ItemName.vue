cf91e7b0f55b33291a904343e3ef6743d65f6e4f
Alexey Diasamidze <diasamidze@bk.ru>
2018-12-27 18:42:18 +0300

    Ok

---
+++ components/ItemName.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="todo-style">
+        <span @click="$emit('choose')" v-if="(todo.id !== chooseId)">
+            {{ todo.name }}
+        </span>
+        <form v-if="(todo.id === chooseId)" @submit.prevent="ChangeName" class="todo-style">
+            <input v-model="newName"/>
+            <input type="submit" value="ok"/>
+            <input type="button" @click="$emit('canceled'); newName = todo.name" value="cancel"/>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["todo", "chooseId"],
+        data() {
+            return {
+                newName: this.todo.name,
+            }
+        },
+        methods: {
+            // @todo
+            ChangeName() {
+                this.$emit("change", this.newName)
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+    .todo-style {
+        display: inline;
+    }
+</style>
---
+++ components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input type="checkbox" v-model="todo.maked"/>
+        <item-name :todo="todo" :chooseId="chooseId" @choose="$emit('choose', todo.id)"
+                       @change="$emit('change', [$event, index])" @canceled="$emit('canceled')"/>
+        <input type="button" value="x" @click="$emit('del')"/>
+    </div>
+</template>
+
+<script>
+    import ItemName from "./ItemName.vue"
+
+    export default {
+        components: {
+            ItemName
+        },
+        props: ["todo", "chooseId", "index"]
+    }
+</script>
